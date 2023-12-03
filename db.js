07613d1bc693d3a28a447503fb68b8ae966c4b90
asn7861 <toanasbinimran@gmail.com>
2023-12-03 21:39:06 +0500

    db url change

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const DB = async () => {
   mongoose
-    .connect("mongodb+srv://dev:mashaq123@ecommerce.bdyrdd0.mongodb.net/")
+    .connect(
+      "mongodb+srv://dev:mashaq123@ecommerce.bdyrdd0.mongodb.net/ecommerce"
+    )
     .then(() => {
       console.log("database connected");
     })
